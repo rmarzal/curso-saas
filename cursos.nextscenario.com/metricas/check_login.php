f99c4a0e6edaab4897c025b40039f4283eaae619
rmarzal <77627461+rmarzal@users.noreply.github.com>
2026-04-24 14:37:51 +0200

    Add files via upload

---
+++ cursos.nextscenario.com/metricas/check_login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+$users = [
+    "magi@funnelcracks.com" => '1M2s45d$marys',
+    "pcorneli@meteosim.com" => 'aert1nesM4',
+    "jserrano.ing@gmail.com" => '1M2s45d$marys',
+    "yaiza_rv@hotmail.com" => '1M2s45d$marys',
+    "joseluis.ojeda@fox.com" => '1M2s45d$marys2',
+    "adrianrodrigo@adrirodrigoagencia.es" => 'asart1n2esR7'
+];
+
+$username = strtolower(trim($_POST['username'] ?? ''));
+$password = trim($_POST['password'] ?? '');
+
+if (isset($users[$username]) && strcmp($users[$username], $password) === 0) {
+    $_SESSION['username'] = $username;
+    header("Location: /metricas/pro/embed.php");
+    exit();
+} else {
+    header("Location: /metricas/login.php?error=1");
+    exit();
+}
+?>
---
+++ cursos.nextscenario.com/metricas/pro/embed_old.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: /metricas/login.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Métricas y Modelos Financieros SaaS</title>
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+        }
+        iframe {
+            display: block;
+            width: 100%;
+            height: 100vh;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+  <iframe src="https://gossamer-cornflower-4c4.notion.site/ebd//21ff1f10e2ba805289a2c8b893026226" width="100%" height="600" frameborder="0" allowfullscreen />
+</body>
+</html>
